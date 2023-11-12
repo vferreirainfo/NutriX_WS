77fa86bc53c0d2f0ce825e60ef853d7ad6112dfc
vferreirainfo <vferreirainfo@gmail.com>
2023-11-12 22:14:29 +0000

    Update :) Foram introduzidas novas classes

---
+++ src/main/java/NurtiX_BO/Pessoa.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NurtiX_BO;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ioutlet
+ */
+public class Pessoa {
+   
+    String nome;
+    String apelido;
+    Date dtnascimento;
+    String email;
+    int indicativoTel;
+    int numTelMovel;
+    char genero; // M ou F
+    String pais; //pais 
+    String morada; //lugar onde mora
+    String prof; 
+    
+    
+}
